f3fcac7bb849380768546a885c1239ac1e8f1c1a
Deba-07 <dynamicdeba07@gmail.com>
2025-01-13 23:49:49 +0530

    hero, header, pricing sections updated

--- saasLandingPage/src/sections/Header.jsx
+++ saasLandingPage/src/sections/Header.jsx
@@ -1,19 +1,23 @@
-import React, { useEffect, useState } from "react";
 import { Link as LinkScroll } from "react-scroll";
+import { useEffect, useState } from "react";
 import clsx from "clsx";
 
 const Header = () => {
-  const [isOpen, setIsOpen] = useState(false);
   const [hasScrolled, setHasScrolled] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
   useEffect(() => {
     const handleScroll = () => {
       setHasScrolled(window.scrollY > 32);
     };
+
     window.addEventListener("scroll", handleScroll);
+
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  });
+  }, []);
+
   const NavLink = ({ title }) => (
     <LinkScroll
       onClick={() => setIsOpen(false)}
@@ -27,6 +31,7 @@ const Header = () => {
       {title}
     </LinkScroll>
   );
+
   return (
     <header
       className={clsx(
@@ -36,20 +41,16 @@ const Header = () => {
     >
       <div className="container flex h-14 items-center max-lg:px-5">
         <a className="lg:hidden flex-1 cursor-pointer z-2">
-          <img
-            src="/images/xora.svg"
-            width={115}
-            height={55}
-            alt="logo"
-          />
+          <img src="/images/xora.svg" width={115} height={55} alt="logo" />
         </a>
+
         <div
           className={clsx(
             "w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0",
             isOpen ? "max-lg:opacity-100" : "max-lg:pointer-events-none"
           )}
         >
-          <div className=" max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:overflow-hidden max-lg:p-6 sidebar-before max-md:px-4">
+          <div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
             <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
               <ul className="flex max-lg:block max-lg:px-12">
                 <li className="nav-li">
@@ -57,6 +58,7 @@ const Header = () => {
                   <div className="dot" />
                   <NavLink title="pricing" />
                 </li>
+
                 <li className="nav-logo">
                   <LinkScroll
                     to="hero"
@@ -68,13 +70,14 @@ const Header = () => {
                     )}
                   >
                     <img
-                      src="/public/images/xora.svg"
-                      height={55}
+                      src="/images/xora.svg"
                       width={160}
+                      height={55}
                       alt="logo"
                     />
                   </LinkScroll>
                 </li>
+
                 <li className="nav-li">
                   <NavLink title="faq" />
                   <div className="dot" />
@@ -82,16 +85,17 @@ const Header = () => {
                 </li>
               </ul>
             </nav>
+
             <div className="lg:hidden block absolute top-1/2 left-0 w-[960px] h-[380px] translate-x-[-290px] -translate-y-1/2 rotate-90">
               <img
-                src="/public/images/bg-outlines.svg"
+                src="/images/bg-outlines.svg"
                 width={960}
                 height={380}
                 alt="outline"
                 className="relative z-2"
               />
               <img
-                src="/public/images/bg-outlines-fill.png"
+                src="/images/bg-outlines-fill.png"
                 width={960}
                 height={380}
                 alt="outline"
@@ -100,12 +104,13 @@ const Header = () => {
             </div>
           </div>
         </div>
+
         <button
           className="lg:hidden z-2 size-10 border-2 border-s4/25 rounded-full flex justify-center items-center"
           onClick={() => setIsOpen((prevState) => !prevState)}
         >
           <img
-            src={`/public/images/${isOpen ? "close" : "magic"}.svg`}
+            src={`/images/${isOpen ? "close" : "magic"}.svg`}
             alt="magic"
             className="size-1/2 object-contain"
           />
--- saasLandingPage/src/sections/Hero.jsx
+++ saasLandingPage/src/sections/Hero.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { Element, Link as LinkScroll } from "react-scroll";
 import Button from "../components/Button.jsx";
 
-function Hero() {
+const Hero = () => {
   return (
-    <section className="relative pt-40 pb-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-md:pb-32">
+    <section className="relative pt-60 pb-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-md:pb-32">
       <Element name="hero">
         <div className="container">
           <div className="relative z-2 max-w-512 max-lg:max-w-388">
@@ -12,19 +12,20 @@ function Hero() {
               Video Editing
             </div>
             <h1 className="mb-6 h1 text-p4 uppercase max-lg:mb-7 max-lg:h2 max-md:mb-4 max-md:text-5xl max-md:leading-12">
-              Amazingly Simple
+              Amazingly simple
             </h1>
             <p className="max-w-440 mb-14 body-1 max-md:mb-10">
               We designed XORA AI Video Editor to be an easy to use, quick to
-              learn and surprisingly powerfull
+              learn, and surprisingly powerful.
             </p>
             <LinkScroll to="features" offset={-100} spy smooth>
-              <Button icon="public/images/zap.svg">Try it now</Button>
+              <Button icon="/images/zap.svg">Try it now</Button>
             </LinkScroll>
           </div>
+
           <div className="absolute -top-32 left-[calc(50%-340px)] w-[1230px] pointer-events-none hero-img_res">
             <img
-              src="/public/images/hero.png"
+              src="/images/hero.png"
               className="size-1230 max-lg:h-auto"
               alt="hero"
             />
@@ -33,6 +34,6 @@ function Hero() {
       </Element>
     </section>
   );
-}
+};
 
 export default Hero;
