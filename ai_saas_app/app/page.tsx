9bac574315fdbf8ce6026ae2103387af0718ccd5
Deba-07 <dynamicdeba07@gmail.com>
2025-07-22 08:47:31 +0530

    update home page

--- ai_saas_app/app/page.tsx
+++ ai_saas_app/app/page.tsx
@@ -1,15 +1,53 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
+import { Button } from "@/components/ui/button";
+import { recentSessions } from "@/constants";
+import React from "react";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-3xl font-bold'>Hello! Welcome to my saas app</h1>
-      <Button>
-        Get started
-      </Button>
-    </div>
-  )
-}
+    <main>
+      <h1>Popular Companions</h1>
 
-export default Page
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+        />
+
+        <CompanionCard
+          id="124"
+          name="Countsy the Number Widzard"
+          topic="Derivatives & Integrals"
+          subject="maths"
+          duration={30}
+          color="#e5D0ff"
+        />
+        <CompanionCard
+          id="125"
+          name="Verba the Vocabulary Builder"
+          topic="English Literature"
+          subject="language"
+          duration={30}
+          color="#bde7ff"
+        />
+      </section>
+
+      <section className="home-section">
+        <CompanionsList
+          title="Recently Completed Sessions"
+          companions={recentSessions}
+          classNames='w-2/3 max-lg:w-full'
+         />
+        <CTA />
+      </section>
+    </main>
+  );
+};
+
+export default Page;
